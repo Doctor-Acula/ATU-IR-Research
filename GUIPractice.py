caba54a2d028b07c07dfeffe4d324ea9d6da9920
Doctor-Acula <109109991+Doctor-Acula@users.noreply.github.com>
2023-01-26 17:54:16 -0600

    Update GUIPractice.py

--- GUIPractice.py
+++ GUIPractice.py
@@ -10,9 +10,9 @@ label1.pack()
 
 #Creates a functioning button
 button1 = tk.Button(
-    text='Press to start',
-    width=25,
-    height=5,
+    text='Do Not Press',
+    width=15,
+    height=2,
     bg="black",
     fg="Red",
 )
@@ -24,15 +24,6 @@ def button1_detection():
     buttonClicked = not buttonClicked
 buttonClicked = True #This may be backwards... Just work with it for now
 
-'''def button1_pressed():
-    if buttonClicked == True:
-        print("Button Pressed")
-        label1.config(text="Button Pressed")
-    else:
-        print("Button Not Pressed")
-        label1.config(text="Button Not Pressed")
-button1.config(command=button1_pressed)'''
-
 def button1_pressed():
     if buttonClicked == True:
         print("Button Pressed")
@@ -41,6 +32,29 @@ def button1_pressed():
         print("Button Not Pressed")
 button1.config(command=button1_pressed)
 
+#Creates another functioning button
+button2 = tk.Button(
+    text='Initiate Thermal Camera',
+    width=30,
+    height=7,
+    bg="black",
+    fg="Red",
+)
+button2.pack()
+
+#Detects when button is pressed
+def button2_detection():
+    global button2Clicked 
+    button2Clicked = not button2Clicked
+button2Clicked = True #This may be backwards... Just work with it for now
+
+def button2_pressed():
+    if button2Clicked == True:
+        print("Button Pressed")
+        os.system('GUIOutput.py')
+    else:
+        print("Button Not Pressed")
+button2.config(command=button1_pressed)
+
 
 window.mainloop()
-
