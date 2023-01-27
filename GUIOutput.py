99ed23ecabb346006eb05e76ffe49cebc60ee588
Doctor-Acula <109109991+Doctor-Acula@users.noreply.github.com>
2023-01-26 19:07:35 -0600

    Update GUIOutput.py

--- GUIOutput.py
+++ GUIOutput.py
@@ -2,8 +2,26 @@ import tkinter as tk
 from tkinter import *
 import time
 import os
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+from pygame import mixer
 
 root = Tk()
+
+w = 600 # width for the Tk root
+h = 425 # height for the Tk root
+
+# get screen width and height
+ws = root.winfo_screenwidth() # width of the screen
+hs = root.winfo_screenheight() # height of the screen
+
+# calculate x and y coordinates for the Tk root window
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+# set the dimensions of the screen 
+# and where it is placed
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 frameCnt = 5
 frames = [PhotoImage(file='pedry.gif',format = 'gif -index %i' %(i)) for i in range(frameCnt)]
